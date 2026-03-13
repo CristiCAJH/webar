bd40bb5393cc4caff3e09ca23403bd772ea54a54
CristiCAJH <ca11joker@yahoo.com>
2026-03-13 22:39:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.log("preinit");
-import * as THREE from "../libs/three.js-r132/build/three.module.js";
+import * as THREE from "./libs/three.js-r132/build/three.module.js";
 
 console.log("THREE", THREE);
 
